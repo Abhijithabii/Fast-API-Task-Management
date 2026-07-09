4aec07e7df3dff6c9c99aa5de7ebf034c0055c25
Abhijithabii <abhijithrajeevan123@gmail.com>
2026-07-09 20:50:37 +0530

    created an update functionality

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer : bool | None = None
 
 @app.get("/")
 def read_root():
@@ -12,4 +18,14 @@ def read_item(item_id:int, q:str | None=None):
     return {
         "Item Id" : item_id,
         "Q" : q
+    }
+
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id:int , item: Item):
+    return {
+        "Item Name" : item.name,
+        "Item Price" : item.price,
+        "Item Id" : item_id
     }
